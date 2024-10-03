39907895715e7747718522138d3f404e3cca6f8a
Mitul <mitmistry311@gmail.com>
2024-10-03 18:41:36 -0400

    removed duplicate footer

--- src/app/doc/DoctorProfile.js
+++ src/app/doc/DoctorProfile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 import Footer from './component/Footer';
 import Sidebar from './component/Sidebar';
@@ -8,29 +8,24 @@ import Reviews from './component/Reviews';
 import Button from './component/Button';
 
 const DoctorProfilePage = () => {
-    return (
-        <div className="relative">
-            {/* Navigation */}
-            {/* <FloatingNav className="fixed top-0 z-50" /> */}
-
-            {/* Doctor Profile */}
-            <div className="flex flex-col h-screen pt-20">
-                <div className="flex flex-grow">
-                    <Sidebar />
-                    <main className="flex-grow p-4">
-                        <Overview />
-                        <Services />
-                        <Reviews />
-                        <div className="text-center flex justify-center space-x-4">
-                            <Button>Send Message</Button>
-                            <Button variant="primary">Make Appointment</Button>
-                        </div>
-                    </main>
-                </div>
-                <Footer />
+  return (
+    <div className='relative'>
+      <div className='flex flex-col h-screen pt-20'>
+        <div className='flex flex-grow'>
+          <Sidebar />
+          <main className='flex-grow p-4'>
+            <Overview />
+            <Services />
+            <Reviews />
+            <div className='text-center flex justify-center space-x-4'>
+              <Button>Send Message</Button>
+              <Button variant='primary'>Make Appointment</Button>
             </div>
+          </main>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DoctorProfilePage;
