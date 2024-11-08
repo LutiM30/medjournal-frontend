5b8a0014599c64c6936dd8237cd5129b740ddf1b
RitenPatel8590 <165744327+RitenPatel8590@users.noreply.github.com>
2024-11-08 06:41:01 +0530

    Added Footer into about us page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -18,21 +18,6 @@ const About = () => {
     return (
         <>
             <div className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
-                {/* grid grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl w-full h-full mx-auto md:auto-rows-[20rem] grid for 3 columns */}
-
-
-                {/* row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 [&>p:text-lg] md:col-span-1 grid for one column part 1 */}
-
-                {/* row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 [&>p:text-lg] md:col-span-2 grid for two columns part 2  */}
-
-
-                {/* <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-                    Welcome to {projectConstants.PROJECT_NAME}
-                </h2> */}
-                {/* <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-                    Sign Up to {projectConstants.PROJECT_NAME}
-                </p> */}
-
                 <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg mg-24">
                     <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
@@ -69,9 +54,8 @@ const About = () => {
                     </h1>
                     <WhyWeBuild />
                 </div>
-
             </div >
-
+            <Footer />
         </>
     );
 };
